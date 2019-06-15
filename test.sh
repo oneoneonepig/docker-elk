9bd891f984fb3ea07b2cc848a1fcd9c543d3908e
oneoneonepig <peihuachu1112@gmail.com>
2019-06-15 20:09:48 +0800

    Add nginx and a whole lot more

---
+++ test.sh
@@ -0,0 +1,22 @@
+
+echo Will >> /var/log/web/who.log
+echo Will >> /var/log/web/who.log
+echo Will >> /var/log/web/who.log
+echo Will >> /var/log/web/who.log
+echo Jeff >> /var/log/web/who.log
+echo Jeff >> /var/log/web/who.log
+echo Jeff >> /var/log/web/who.log
+echo Jeff >> /var/log/web/who.log
+echo Jeff >> /var/log/web/who.log
+echo Dora >> /var/log/web/who.log
+echo Dora >> /var/log/web/who.log
+echo Dora >> /var/log/web/who.log
+
+curl -s localhost:8080
+curl -s localhost:8080
+curl -s localhost:8080
+curl -s localhost:8080
+curl -s localhost:8080/a
+curl -s localhost:8080/b
+curl -s localhost:8080/c
+
